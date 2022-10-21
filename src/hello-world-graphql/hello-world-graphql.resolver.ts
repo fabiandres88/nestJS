5dd8e538244bfa59a4fd76658393227904ccb5c2
FABIAN RAMIREZ <fabianramirez@FABIANs-MacBook-Pro.local>
2022-10-21 12:39:15 -0500

    feat: add query gql for dice

--- src/hello-world-graphql/hello-world-graphql.resolver.ts
+++ src/hello-world-graphql/hello-world-graphql.resolver.ts
@@ -2,8 +2,13 @@ import { Query, Resolver } from '@nestjs/graphql';
 
 @Resolver()
 export class HelloWorldGraphqlResolver {
-    @Query(() => String, {name: "message"})
+    @Query(() => String, { name: "message" })
     getHelloWorld(): string {
         return "Hello world graph-ql";
+    }
+
+    @Query(() => Number, { name: "randomDice" })
+    getRandomNmber(): number {
+        return 10;
     }
 }
